0b9623335f22416ed1ceedb72285c923e526c06f
Justin McDonald <73570812+jmcdonald112358@users.noreply.github.com>
2021-02-23 02:09:21 -0700

    Removed index2.js and updated index.js with appropriately functioning async code to generate file in the desired order

--- index.js
+++ index.js
@@ -2,270 +2,188 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-//Define filename
-const filename = "newREADME.md";
+//Object containing license info
+const licences = {
+   "MIT License": {
+      badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+   },
+   "ISC License": {
+      badge: '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
+   },
+   "Apache License 2.0": {
+      badge: '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+   },
+   "GNU GPLv3": {
+      badge: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+   }
+};
 
 //Prompts for info about the project
 const questions = [
-      //Add another confirm that shows if a README file already exists -- prompt whether or not to empty contents
-   
-      //Project title
-      //Q1
-      {
-         type: 'input',
-         message: 'What is the name of the project?',
-         name: 'projectTitle',
-         validate: function (input) {
-            if (input.length < 5) {
-               return 'Project title must be greater than 5 characters.';
-            }
-            return true;
+
+   //Project title
+   {
+      type: 'input',
+      message: 'What is the name of the project?',
+      name: 'projectTitle',
+      validate: function (input) {
+         if (input.length < 5) {
+            return 'Project title must be greater than 5 characters.';
          }
-      },
-   
-   //Description
-      //Q2
-      {
-         type: 'input', //Change back to 'editor' and re-enable validation once testing is done
-         message: 'Enter a description of the project that is at least 3 lines:',
-         name: 'projectDescription',
-         default: 'Edit contents here (removing these instructions), then save and close window to return to the program and continue.',
-         // validate: function (text) {
-         //    if (text.split('\n').length < 3) {
-         //       return 'Description must be at least 3 lines.';
-         //    }
-         //    return true;
-         // }
-      },
-
-   //Table of contents
-      //Links to other sections of README
-      //This isn't input by user -- it gets generated from the functionality itself, but prompt user to include or not
-      //Q3
-      {
-         type: 'confirm',
-         message: 'Do you want a table of contents?',
-         name: 'includeTOC',
-      },
-
-
-   //Installation instructions
-      //Q4
-      {
-         type: 'input', //Change back to 'editor' if deemed necessary
-         message: 'Provide instructions for installing the project. Use markdown formatting for multi-step instructions if necessary.',
-         default: 'Edit contents here (removing these instructions), then save and close window to return to the program and continue.',
-         name: 'installation'
-      },
-
-   //Usage
-      //Q5
-      {
-         type: 'input',//Change back to 'editor' if deemed necessary
-         message: 'Provide instructions for using the application. Use markdown formatting for multi-step instructions and linking screenshots.',
-         default: 'Edit contents here (removing these instructions), then save and close window to return to the program and continue.',
-         name: 'usage'
-      },
-
-
-   //License info (list of options)
-      //Populate corresponding badge and notice; include a 'none' option
-      //Q6
-      {
-         type: 'checkbox',
-         message: 'What licence(s) should be referenced? Select "None" if the desired option is not listed and manually add it later.',
-         name: 'license',
-         choices: [
-            {
-               name: 'MIT License',
-               checked: true
-            },
-            {
-               name: 'ISC License'
-            },
-            {
-               name: 'Apache License 2.0'
-            },
-            {
-               name: 'GNU GPLv3'
-            },
-            {
-               name: 'None'
-            }
-         ],
-         // validate: function (answer) {
-         //    if (answer.length > 1 && answer[4].checked === true) {
-         //       return '"None" cannot be selected when one or more other selections are active.'
-         //    }
-         //    return true
-         // }
-      },
-
-
-   //Contribution guidelines
-      //Q7
-      {
-         type: 'input', //Change back to 'editor' if deemed necessary
-         message: 'Provide details on how others can contribute to the project. ',
-         name: 'contribution',
-         default: 'Please email me at {enter your email} if you would like to contribute' 
-      },
-
-   //Credits
-      //Q8
-      {
-         type: 'confirm',
-         message: 'Do you have any collaborators or other attributions to acknowledge?',
-         name: 'confirmCredits'
-      },   
-   
-
-      //Q9
-      {
-         type: 'input', //Change back to 'editor' if deemed necessary
-         message: 'List collaborators with links to their GitHub profiles, attribute third-party assets and their creators, and link any tutorials or other resources.',
-         default: 'Edit contents here (removing these instructions), then save and close window to return to the program and continue.',
-         name: 'credits',
-         when: function (data) {
-            return data.confirmCredits;
-         }
-      },
-
-
-   //Test instructions
-      //Q10
-      {
-         type: 'confirm',
-         message: 'Does the project have test instructions? (y/N)',
-         name: 'confirmTests'
-      },   
-
-      //Q11
-      {
-         type: 'input', //Change back to 'editor' if deemed necessary
-         message: 'Add instructions for testing the project. ',
-         default: 'Edit contents here (removing these instructions), then save and close window to return to the program and continue.',
-         name: 'testInstructions',
-         when: function (data) {
-            return data.confirmTests;
-         }
-      },
-
-   //Questions section
-      //Q12
-      {
-         type: 'prompt',
-         message: 'Enter your GitHub username: ',
-         name: 'username'
-      },
-
-      //Q13
-      {
-         type: 'prompt',
-         message: 'Enter email address users and contributers can reach you at: ',
-         name: 'email'
+         return true;
       }
+   },
+
+//Description
+   {
+      type: 'input',
+      message: 'Enter the description of the project: ',
+      name: 'projectDescription',
+      default: 'Type description and press enter to continue...',
+   },
+
+//Table of contents
+   {
+      type: 'confirm',
+      message: 'Do you want a table of contents?',
+      name: 'includeTOC',
+   },
+
+
+//Installation instructions
+   {
+      type: 'input', 
+      message: 'Provide instructions for installing the project: ',
+      name: 'installation',
+      default: 'Type installation instructions here, using markdown formatting if necessary, and press enter to continue...'
+   },
+
+//Usage
+   {
+      type: 'editor',
+      message: 'Provide instructions for using the application: ',
+      default: 'Edit contents here, including removing these instructions, using markdown for formatting multi-step usage instructions and inserting screenshots, then save and close window to return to the program and continue.',
+      name: 'usage'
+   },
+
+
+//License info (list of options)
+   {
+      type: 'checkbox',
+      message: 'What licence(s) should be referenced? Select "None" if the desired option is not listed and manually add it later.',
+      name: 'license',
+      choices: [
+         {
+            name: 'MIT License',
+            checked: true
+         },
+         {
+            name: 'ISC License'
+         },
+         {
+            name: 'Apache License 2.0'
+         },
+         {
+            name: 'GNU GPLv3'
+         },
+         {
+            name: 'None'
+         }
+      ],
+   },
+
+
+//Contribution guidelines
+   {
+      type: 'input', 
+      message: 'Provide details on how others can contribute to the project: ',
+      name: 'contribution',
+      default: 'Type instructions for contributing to the project, using markdown formatting if necessary, and press enter to continue...' 
+   },
+
+//Credits
+   {
+      type: 'confirm',
+      message: 'Do you have any collaborators or other attributions to acknowledge?',
+      name: 'confirmCredits'
+   },   
+
+
+   {
+      type: 'editor',
+      message: 'Provide details on applicable credits: ',
+      default: 'Edit contents here, including removing these instructions, then save and close window to return to the program and continue. List collaborators with links to their GitHub profiles, attribute third-party assets and their creators, and link any tutorials or other resources. Use markdown to format content and insert links if necessary.',
+      name: 'credits',
+      when: function (input) {
+         return input.confirmCredits;
+      }
+   },
+
+
+//Test instructions
+   {
+      type: 'confirm',
+      message: 'Does the project have test instructions?',
+      name: 'confirmTests'
+   },   
+
+   {
+      type: 'input', 
+      message: 'Add instructions for testing the project: ',
+      default: 'Type instructions here, using markdown if necessary, then press enter to continue...',
+      name: 'testInstructions',
+      when: function (input) {
+         return input.confirmTests;
+      }
+   },
+
+//Questions section
+   {
+      type: 'prompt',
+      message: 'Enter your GitHub username: ',
+      name: 'username'
+   },
+
+   {
+      type: 'prompt',
+      message: 'Enter email address users and contributers can reach you at: ',
+      name: 'email'
+   }
 ]
 
 //Function to write README file
-function writeFile(data) {
+function generate(input) {
+
+   const { projectTitle, projectDescription, includeTOC, installation, usage, license, contribution, confirmCredits, credits, confirmTests, testInstructions, email, username } = input;
+
+   //Code for generating license badges, pulled out of the template literal in order to be commented out appropriately
+   // ${license.forEach(element => {
+   //    return licenses[element].badge;
+   // })}\n
+
+   return `# ${projectTitle}\n
    
-   console.log(data);
+   ### Description \n ${projectDescription}\n
 
-//Functioning but not quite right version (works, but puts things out of order due to no async/promises)
-   //Append title
-   fs.appendFile(filename, 
-      `# ${JSON.parse(JSON.stringify(data.projectTitle))}\n`, 
-      (err) => {
-         (err) ? console.error(err) : console.log("Title added successfully!")
-      }
-   );
+   ${includeTOC && `### Table of Contents \n - [Installation](#installation)\n - [Usage](#usage)\n - [License](#license)\n - [How to contribute](#contribute)\n - [Credits (conditional)](#credits)\n - [Test instructions (conditional)](#tests)\n - [Questions](#questions)\n\n`}
 
-   //Append description
-   fs.appendFile(filename, 
-      `### Description \n ${JSON.parse(JSON.stringify(data.projectDescription))}\n`, 
-      (err) => {
-         (err) ? console.error(err) : console.log("Description added successfully!")
-      }
-   );
 
-   //Append TOC if confirm is true
-   if (data.includeTOC === true) {
-      fs.appendFile(filename, 
-         `### Table of Contents \n - [Installation](#installation)\n - [Usage](#usage)\n - [License](#license)\n - [How to contribute](#contribute)\n - [Credits (conditional)](#credits)\n - [Test instructions (conditional)](#tests)\n - [Questions](#questions)\n `, 
-         (err) => {
-            (err) ? console.error(err) : console.log("TOC generated successfully!")
-         }
-      )
-   };
+### Installation \n ${installation}\n
+   ### Usage \n ${usage}\n
+### License \n Licensed under the following: ${license.join(', ')}\n
+   ### Contribute \n ${contribution}\n
 
-   //Append installation instructions
-   fs.appendFile(filename, 
-      `### Installation \n ${JSON.parse(JSON.stringify(data.installation))}\n`, 
-      (err) => {
-         (err) ? console.error(err) : console.log("Installation instructions added successfully!")
-      }
-   );
+   ${confirmCredits ? `### Credits \n ${credits}\n` : ''}
 
-   //Append usage instructions
-   fs.appendFile(filename, 
-      `### Usage \n ${JSON.parse(JSON.stringify(data.usage))}\n`, 
-      (err) => {
-         (err) ? console.error(err) : console.log("Usage instructions added successfully!")
-      }
-   );
-
-   //Append licence info
-   fs.appendFile(filename, 
-      `### License \n Licensed under the following: ${JSON.parse(JSON.stringify(data.license.join(', ')))}\n`, 
-      (err) => {
-         (err) ? console.error(err) : console.log("License info added successfully!")
-      }
-   );
-
-   //Append contribution instructions
-   fs.appendFile(filename, 
-      `### Contribute \n ${JSON.parse(JSON.stringify(data.contribution))}\n`, 
-      (err) => {
-         (err) ? console.error(err) : console.log("Contribution instructions added successfully!")
-      }
-   );
-
-   //Append credits if confirm is true
-   if (data.confirmCredits === true) {
-      fs.appendFile(filename, 
-         `### Credits \n ${JSON.parse(JSON.stringify(data.credits))}\n`, 
-         (err) => {
-            (err) ? console.error(err) : console.log("Credits added successfully!")
-         }
-      );
-   } 
+   ${confirmTests ? `### Tests \n ${testInstructions}\n` : ''}
    
-   //Append test instructions if confirm is true
-   if (data.confirmTests === true) {
-      fs.appendFile(filename, 
-         `### Tests \n ${JSON.parse(JSON.stringify(data.testInstructions))}\n`, 
-         (err) => {
-            (err) ? console.error(err) : console.log("Test instructions added successfully!")
-         }
-      );
-   }
-
-   //Append questions
-   fs.appendFile(filename, 
-      `### Questions \n If you have any questions about the project, please feel free to message at **${JSON.parse(JSON.stringify(data.email))}**, or connect with me on GitHub: **[${JSON.parse(JSON.stringify(data.username))}](https://github.com/${JSON.parse(JSON.stringify(data.username))})** \n`, 
-      (err) => {
-         (err) ? console.error(err) : console.log("Questions section added successfully!")
-      }
-   );
-
+### Questions \n If you have any questions about the project, please feel free to message at **${email}**, or connect with me on GitHub: **[${username}](https://github.com/${username})** \n`
 }
 
-//Function to initialize program
-function init() {
-   inquirer.prompt(questions).then((data) => {
-      writeFile(data);
-   })
+//Function to initialize program 
+async function init() {
+   const input = await inquirer.prompt(questions);
+   fs.writeFile("README.md", generate(input), (err) => err && console.error(err));
 }
 
 //Function call to initialize program
